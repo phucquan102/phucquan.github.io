f4afa0b5979f016de7c6391435e948f520318942
phucquan102 <ngophucquan2002@gmail.com>
2024-03-22 16:06:26 +0700

    up

--- lab3/routes/index.js
+++ lab3/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hn', function(req, res, next) {
+  res.render('greenwich/hn' );
+});
+
+router.get('/hcm', function(req, res, next) {
+  res.render('greenwich/hcm' );
+});
+
+router.get('/dn', function(req, res, next) {
+  res.render('greenwich/dn' );
+});
+
+router.get('/ct', function(req, res, next) {
+  res.render('greenwich/ct' );
+});
+
 module.exports = router;
